a285a59e81d37c1cb56184ad20d5fec435970f65
John <liljorgy@gmail.com>
2022-06-11 09:25:26 -0400

    Adding a number of files related to the backend connection. Those files are: - conn.js - record.js - backend.js - config.env - recordList.js
    The recordList.js file is not final, this is a file to test how to get the database records.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import HomePage from "./components/HomePage";
+import { Route, Routes } from 'react-router-dom';
+import RecordList from './components/recordList';
 
 function App() {
-
-
 return (
-    <HomePage />
+    <div>
+      <HomePage />
+      <Routes>
+        <Route exact path="/test" element={<RecordList />} />
+      </Routes>
+    </div>
   );
 }
 
